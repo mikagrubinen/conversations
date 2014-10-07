689f0c7a4ed2682ec349c173fd81dae060ec4ef6
Miroslav Grubic <mikagrubinen@yahoo.com>
2014-10-07 14:10:30 -0700

    testing new laptop. no changes

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 /* 
+ * Ovo je proba za novi git!
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
